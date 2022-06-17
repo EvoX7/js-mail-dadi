7f8969026ba017aa35e2b488561dec34dba9c1df
EvoX7 <creativesoultouch@gmail.com>
2022-06-17 20:21:26 +0200

    Dice roll milestone

--- gioco-dadi/js/main.js
+++ gioco-dadi/js/main.js
@@ -4,6 +4,61 @@
 
 
 
+// Defining variables for player and computer 
+
+// Player:
+const player = ("Player");
+
+// Computer:
+const computer = ("Computer");
+
+// Defining how many roll sides 
+let diceRoll;
+
+// Defining how many chanches player and computer have with each sides 
+let randomNumberP = Math.ceil(Math.random() * 6);
+let randomNumberC = Math.ceil(Math.random() * 6);
+
+
+// Rolling the dice using js loop 
+for (let diceRoll = 1; diceRoll <= 6; diceRoll++) {
+
+    // If numbers are equal, there is a draw
+    if (randomNumberP === randomNumberC) {
+        document.querySelector("h2").innerHTML = "It's a Draw!";
+        console.log("It's a Draw!");
+    }
+
+    // If computer's result is minor to player's, there is a win for the player 
+    else if (randomNumberC < randomNumberP) {
+        document.querySelector("h2").innerHTML = (player + " WINS!");
+        document.querySelector("h4").innerHTML = (randomNumberP);
+        console.log(player + " " + "WINS!");
+
+    }
+
+    // If player's result is minor to computer's, there is a win for the computer
+    else {
+        document.querySelector("h2").innerHTML = (computer + " WINS!");
+        document.querySelector("h4").innerHTML =  (randomNumberC);
+        console.log(computer + " " + "WINS!");
+    }
+
+}
+
+console.log(computer + " " + randomNumberC);
+console.log(player + " " + randomNumberP);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
