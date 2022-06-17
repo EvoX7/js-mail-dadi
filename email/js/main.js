d786f8e58c6aabd1f64c3d6baa03d2e140411366
EvoX7 <creativesoultouch@gmail.com>
2022-06-17 14:04:48 +0200

    Starting with email

---
+++ email/js/main.js
@@ -0,0 +1,17 @@
+// Mail
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+// Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+
+// Asking user email 
+const userMail = prompt ("Enter your email address to sign-in");
+console.log (userMail);
+
+
+// My Array 
+const MailList = ["splintercell@ubisoft.com", "wonderwoman@gmail.com", "marvelous753@tesla.com", "lore17@gmail.com", "elden-ring@fromsoftware.com", "masseffect@bioware.com", "thelastofus2@naughtydog.com", "uncharted@naughtydog.com", "ghostwiretokyo6@tangogameworks.com", "hellblade2@ninjatheory.com", "godofwar2022@santamonicastudio.com", "bioshock@g2kgames.com", "residentevil8@capcom.com", "watchdogs3@ubisoft.com", "callofduty@acivision.com",];
+console.log (MailList);
+
+// Check email 
+if ()
+
