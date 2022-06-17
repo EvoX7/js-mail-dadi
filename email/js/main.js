c10a291cd8a1cc0a1782ef841f876b05f826f979
EvoX7 <creativesoultouch@gmail.com>
2022-06-17 15:02:07 +0200

    Exercise 1 - email completed

--- email/js/main.js
+++ email/js/main.js
@@ -4,14 +4,38 @@
 
 
 // Asking user email 
-const userMail = prompt ("Enter your email address to sign-in");
-console.log (userMail);
+const userMail = prompt("Enter your email address to sign-in");
+console.log(userMail);
 
 
 // My Array 
-const MailList = ["splintercell@ubisoft.com", "wonderwoman@gmail.com", "marvelous753@tesla.com", "lore17@gmail.com", "elden-ring@fromsoftware.com", "masseffect@bioware.com", "thelastofus2@naughtydog.com", "uncharted@naughtydog.com", "ghostwiretokyo6@tangogameworks.com", "hellblade2@ninjatheory.com", "godofwar2022@santamonicastudio.com", "bioshock@g2kgames.com", "residentevil8@capcom.com", "watchdogs3@ubisoft.com", "callofduty@acivision.com",];
-console.log (MailList);
+const mailList = ["splintercell@ubisoft.com", "wonderwoman@gmail.com", "marvelous753@tesla.com", "lore17@gmail.com", "elden-ring@fromsoftware.com", "masseffect@bioware.com", "thelastofus2@naughtydog.com", "uncharted@naughtydog.com", "ghostwiretokyo6@tangogameworks.com", "hellblade2@ninjatheory.com", "godofwar2022@santamonicastudio.com", "bioshock@g2kgames.com", "residentevil8@capcom.com", "watchdogs3@ubisoft.com", "callofduty@acivision.com",];
+console.log(mailList);
 
-// Check email 
-if ()
+
+// Check and message 
+let mailCheck = false;
+let message = ("Couldn't find your email");
+
+
+
+// Check email with loop
+for (let myArray = 0; myArray < mailList.length; myArray++) {
+    if (userMail === mailList[myArray]) {
+
+        mailCheck = true;
+        message = ("Access granted");
+
+    }
+
+}
+
+// Check and message output 
+console.log(mailCheck);
+alert(message);
+
+
+
+
+
 
